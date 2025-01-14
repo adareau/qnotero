3d196efe3982f51578ab33d4d24748f5a7ed5805
Alexandre Dareau <alexandre.Dareau@gmail.com>
2025-01-14 18:56:46 +0100

    MVP for sending to remarkable via local webserver

--- libqnotero/qnoteroResults.py
+++ libqnotero/qnoteroResults.py
@@ -18,10 +18,81 @@
 
 from libqnotero.qt.QtGui import QListWidget, QInputDialog
 from libqnotero.qt.QtCore import Qt
+from pathlib import Path
 import subprocess
 import os
 import platform
+import requests
 
+# GLOBAL VARIABLES
+REMARKABLE_IP = "10.11.99.1"
+DOCUMENT_LIST_REQUEST = "http://{ip}/documents/{guid}"
+DOCUMENT_UPLOAD_REQUEST = "http://{ip}/upload"
+
+# FUNCTIONS FOR REMARKABLE
+
+def select_directory(guid=""):
+    global REMARKABLE_IP, DOCUMENT_LIST_REQUEST
+    req_str = DOCUMENT_LIST_REQUEST.format(ip=REMARKABLE_IP, guid=guid)
+    try:
+        response = requests.get(req_str, timeout=5)
+    except requests.exceptions.Timeout:
+        return 0
+    return response.status_code
+
+
+def upload_file(fpath):
+    global DOCUMENT_UPLOAD_REQUEST
+
+    # Prepare request headers and files
+    headers = {
+        "Origin": f"http://{REMARKABLE_IP}",
+        "Accept": "*/*",
+        "Referer": f"http://{REMARKABLE_IP}/",
+        "Connection": "keep-alive",
+    }
+    files = {
+        "file": (fpath.name, fpath.open("rb"), "application/pdf"),
+    }
+
+    # Send request
+
+    response = requests.post(
+        DOCUMENT_UPLOAD_REQUEST.format(ip=REMARKABLE_IP), headers=headers, files=files
+    )
+
+    return response
+
+def send_to_remarkable(fpath):
+    # if not pdf >> pass
+    if fpath.suffix != ".pdf":
+        msg = f"[Send to RK] Bad file extension for {fpath.name}: \n transfer only works for pdf files."
+        zenity_notif(msg)
+        return
+
+    # check remarkable
+    if select_directory() != 200:
+        msg = f"[Send to RK] Cannot connect to Remarkable. \n Check that it is accessible at http://{REMARKABLE_IP}"
+        zenity_notif(msg)
+        return
+
+    # upload
+    response = upload_file(fpath)
+    if response.status_code == 201:
+        msg = f"[Send to RK] {fpath.name} uploaded to Remarkable !"
+        zenity_notif(msg)
+    else:
+        msg = f"[Send to RK] ERROR Uploading {fpath.name} : (status {response.status_code})!"
+        zenity_notif(msg)
+    return
+
+def zenity_notif(msg=""):
+    try:
+        r = subprocess.run(["zenity", "--notification", "--text", msg])
+    except:
+        pass
+
+# ----------------------------------------------------------
 
 class QnoteroResults(QListWidget):
     """The Qnotero result list"""
@@ -140,6 +211,12 @@ class QnoteroResults(QListWidget):
             self.SendByMail(item)
             return
 
+        # AD : pressing 'r' transfers to remarkable
+        elif (e.key() == Qt.Key_R):
+            item = self.currentItem()
+            self.SendToRemarkable(item)
+            return
+        
         QListWidget.keyPressEvent(self, e)
 
 
@@ -234,6 +311,57 @@ class QnoteroResults(QListWidget):
         except Exception as exc:
             print("qnoteroResults.OpenXournalpp(): failed to open file, sorry... %s" % exc)
         self.qnotero.popDown()
+
+
+    def SendToRemarkable(self, item):
+        """
+        Transfers the item to remarkable via local webserver
+        https://remarkable.guide/tech/usb-web-interface.html#api
+
+        Arguments:
+        item -- a qnoteroItem
+        """
+        #TODO : refactor with DoubleClicked()
+        if item is None or not hasattr(item, "zoteroItem"):
+            return
+        zoteroItem = item.zoteroItem
+        if zoteroItem.fulltext is None and zoteroItem.url is None:
+            return
+        # If there is no a fulltext item open the URL of the entry
+        if len(zoteroItem.fulltext) == 0:
+            path = zoteroItem.url
+        # Only one attachment
+        elif len(zoteroItem.fulltext) == 1:
+            path = zoteroItem.fulltext[0]
+        # If there are more than one
+        else:
+            path, okPressed = QInputDialog.getItem(self, u'Attachments',
+                                                   u'Select attachment to open:', zoteroItem.fulltext, 0, False)
+            if path is None or not okPressed:
+                return
+
+        try:
+            if platform.system() == 'Darwin':  # macOS
+                subprocess.call(('open', path))
+            elif platform.system() == 'Windows':  # Windows
+                path = os.path.normpath(path)
+                os.startfile(path)
+            else:  # linux variants
+                # QUICKFIX (A. Dareau) ----------------------------------------
+                # for some reason, the "fulltext" attribute starts
+                # with 'attachments:', followed by the path relative to the
+                # current user home folder (in my case at least). I fix
+                # that by replacing 'attachments:' by the current user home
+                path = self._attached_files_path(path)
+                # --------------------------------------------------------------
+                fpath = Path(path)
+                send_to_remarkable(fpath)
+            print("qnoteroResults.DoubleClicked(): file opened")
+        except Exception as exc:
+            print("qnoteroResults.DoubleClicked(): failed to open file or URL, sorry... %s" % exc)
+
+        self.qnotero.popDown()
+
 
     def Clicked(self, item):
         if item is None or not hasattr(item, "zoteroItem"):
